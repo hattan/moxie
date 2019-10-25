1d534283ecf385c0f2f2e7f86f99cf9435f76f8d
Hattan Shobokshi <hattan@gmail.com>
2019-10-24 17:13:35 -0700

    adding main.gos

---
+++ temp/main/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"temp/log"
+	"fmt"
+	"os"
+)
+
+var logger log.Logger
+
+func main(){
+	logger, err := log.NewLogger(log.Red)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	logger.Log("Hi There!")	
+}
